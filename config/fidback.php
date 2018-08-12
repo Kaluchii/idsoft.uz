d4004c3ccf03e93bcc8b0b831e766d691f5a36c0
Kaluchii <mr.kaluchii@mail.ru>
2018-08-12 21:26:45 +0600

    Change alt text

--- config/fidback.php
+++ config/fidback.php
@@ -2,11 +2,11 @@
 
 return [
     'block_name' => 'fidback',
-    'mail_username' => 'demigod96@bk.ru',
-    'site_name' => 'IDsoft.kz',
-    'mail_rec' => 'sales@idsoft.info',
-    'mail_rec_copy1' => 'demigod96@bk.ru',
-    'mail_rec_copy2' => 'demigod96@bk.ru',
-    'mail_rec_copy3' => 'demigod96@bk.ru',
+    'mail_username' => 'interpro@idsoft.kz',
+    'site_name' => 'IDsoft.uz',
+    'mail_rec' => 'info@idsoft.uz',
+    'mail_rec_copy1' => 'info@idsoft.uz',
+    'mail_rec_copy2' => 'info@idsoft.uz',
+    'mail_rec_copy3' => 'info@idsoft.uz',
     'inqueue' => false,
 ];
